c10dc4e4d93c8a0ad4504d5965eec5ee73aad86a
waseem tasawar <waseemtasawar97@gmail.com>
2024-05-26 00:43:05 +0500

    UPdate App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
+        {reactDescriptions[genRandomInt(2)]} React concepts you will need for
+        almost any app you are going to build!
       </p>
     </header>
   );
