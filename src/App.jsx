fb030d8dbe7622c2b441ecf1e8f5182fe6310b35
waseem tasawar <waseemtasawar97@gmail.com>
2024-05-31 00:13:38 +0500

    update content. make it dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -19,17 +19,9 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreComponent
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
-
-            <CoreComponent {...CORE_CONCEPTS[1]} />
-
-            <CoreComponent {...CORE_CONCEPTS[2]} />
-
-            <CoreComponent {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((componentItems) => (
+              <CoreComponent key={componentItems.title} {...componentItems} />
+            ))}
           </ul>
         </section>
         <section id="examples">
